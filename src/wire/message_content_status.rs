ceea6d5487e45f38d299453e61e1250eaef7ca54
thehappycheese <the.nicholas.archer@gmail.com>
2024-01-21 11:54:44 +0800

    improve docs

--- src/wire/message_content_status.rs
+++ src/wire/message_content_status.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 
+/// See docs for [StatusPublication]
 #[derive(Serialize, Deserialize, Debug, Default, PartialEq)]
 #[serde(rename_all="snake_case")]
 pub enum ExecutionState {
@@ -18,8 +19,11 @@ pub enum ExecutionState {
 /// When a kernel receives a request and begins processing it, the kernel shall immediately publish
 /// a status message with execution_state: 'busy'. When that kernel has completed processing the
 /// request and has finished publishing associated IOPub messages, if any, it shall publish a status
-///  message with execution_state: 'idle'. Thus, the outputs associated with a given execution shall
+/// message with execution_state: 'idle'. Thus, the outputs associated with a given execution shall
 /// generally arrive between the busy and idle status messages associated with a given request.
+/// 
+/// The `starting` status is supposed to be sent just once at startup, but I have not seen it used
+/// by ipython or evcxr
 #[derive(Serialize, Deserialize, Debug, Default, PartialEq)]
 pub struct StatusPublication {
     /// When the kernel starts to handle a message, it will enter the 'busy'
